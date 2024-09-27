b36f496aecbc88106d00513a4face40526248dc1
m4ripaiva <160244272+m4ripaiva@users.noreply.github.com>
2024-09-27 08:12:14 -0300

    Atualização_v1

--- principal.js
+++ principal.js
@@ -12,6 +12,28 @@ var altura = tdAltura.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
+var tdimc = paciente.querySelector(".info-imc");
+
 var imc = peso / (altura * altura);
 
-console.log(imc);
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+    pesoEhValido = false;
+    tdimc.textContent = "Peso Inválido!";
+}
+
+if (altura <= 0 || altura >= 1000) {
+    alturaEhValido = false;
+    tdimc.textContent = "Altura inválida";
+
+    if (pesoEhValido && alturaEhValida) {
+        var imc = peso / (altura * altura);
+        tdimc.textContent = imc;
+    }
+}
+//console.log(imc);
